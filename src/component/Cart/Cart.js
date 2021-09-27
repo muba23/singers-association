b045b8f159beaeb8bdb9ea4033d4a93da0db2113
muba23 <muba2307@gmail.com>
2021-09-27 22:39:45 +0600

    last push

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -6,18 +6,22 @@ const Cart = (props) => {
     let total = 0;
     for(const singer of cart){
         total = (total  + singer.price);
+        let name = [];
+        name.push(props.name);
     }
-    let name = [];
-    name.push(props.name);
-    // name.map(name=>)
+    
     return (
         <div className="cart-section">
             <h4 className="arrangement">Arrangement of singers</h4>
             <h5>Singers Added: {props.cart.length}</h5>
             <br />
-            {/* <h5>Name: {props.cart.name} </h5> */}
-            
-            <h6>Total Budget: ${total.toFixed(2)}</h6>
+            <ul className="list">
+                <h5>Name:
+                    {
+                
+                   }</h5>
+                <h5>Total Budget: ${total.toFixed(2)}</h5>
+            </ul>
         </div>
     );
 };
