14c6a0c7a5da1943bc1308446975c9b296ced1ae
muba23 <muba2307@gmail.com>
2021-09-27 22:12:23 +0600

    ReadMe file added

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Information from '../Information/Information';
 import './Cart.css'
 
 const Cart = (props) => {
@@ -8,14 +7,16 @@ const Cart = (props) => {
     for(const singer of cart){
         total = (total  + singer.price);
     }
-    // let name = [props.name];
+    let name = [];
+    name.push(props.name);
+    // name.map(name=>)
     return (
         <div className="cart-section">
             <h4 className="arrangement">Arrangement of singers</h4>
             <h5>Singers Added: {props.cart.length}</h5>
             <br />
-            <h5> <Information info={props.info}></Information></h5>
             {/* <h5>Name: {props.cart.name} </h5> */}
+            
             <h6>Total Budget: ${total.toFixed(2)}</h6>
         </div>
     );
